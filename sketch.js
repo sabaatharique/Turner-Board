539667e0fc7d4f92a0253ceed81a070daee5a87a
sabaatharique <saba.5.9.2003@gmail.com>
2024-08-06 13:46:11 +0400

    whac-a-mole game added

--- sketch.js
+++ sketch.js
@@ -1,5 +1,13 @@
 let width = 4*window.innerWidth/5;
-let height = 4*window.innerHeight/5;
+let height = 2*window.innerHeight/3;
+
+//game stuff
+let play = false;
+let playBtn;
+let result = 0;
+let currentTime = 0;
+let timer = null;
+let countdownTimer;
 
 //sounds
 let eng_sheff;
@@ -12,7 +20,7 @@ let rock_n_roll;
 let stupid_pc;
 let yawned;
 
-//buttons
+//buttons array
 let buttons_top = [];
 let buttons_middle = [];
 let buttons_bottom = [];
@@ -26,6 +34,7 @@ class Button {
     this.colour = colour;
     this.accent = accent;
     this.sound = sound;
+    this.mole = false;
   }
 
   show() {
@@ -48,6 +57,11 @@ class Button {
     if (d < this.w/3) {
       this.y = this.y + 10;
       this.sound.play();
+      if (play && this.mole){
+        result++;
+        const score = document.querySelector('#score');
+        score.textContent = result;
+      }
     }
   }
 }
@@ -73,17 +87,17 @@ function setup() {
   yawned = loadSound('sounds/yawned.mp3');
 
   //create buttons
-  let btn1 = new Button(width/4, height/4, height/3, height/8, color(200, 65, 75), color(230, 115, 120), eng_sheff);
-  let btn2 = new Button(width/2, height/4, height/3, height/8, color(200, 65, 75), color(230, 115, 120), fkn_words);
-  let btn3 = new Button(3*width/4, height/4, height/3, height/8, color(200, 65, 75), color(230, 115, 120), ghost_cookies);
+  let btn1 = new Button(width/4, height/4-30, height/3, height/8, color(200, 65, 75), color(230, 115, 120), eng_sheff);
+  let btn2 = new Button(width/2, height/4-30, height/3, height/8, color(200, 65, 75), color(230, 115, 120), fkn_words);
+  let btn3 = new Button(3*width/4, height/4-30, height/3, height/8, color(200, 65, 75), color(230, 115, 120), ghost_cookies);
 
-  let btn4 = new Button(width/4, height/2, height/3, height/8, color(200, 65, 75), color(230, 115, 120), huevos);
-  let btn5 = new Button(width/2, height/2, height/3, height/8, color(200, 65, 75), color(230, 115, 120), ladies);
-  let btn6 = new Button(3*width/4, height/2, height/3, height/8, color(200, 65, 75), color(230, 115, 120), not_quite);
+  let btn4 = new Button(width/4, height/2-30, height/3, height/8, color(200, 65, 75), color(230, 115, 120), huevos);
+  let btn5 = new Button(width/2, height/2-30, height/3, height/8, color(200, 65, 75), color(230, 115, 120), ladies);
+  let btn6 = new Button(3*width/4, height/2-30, height/3, height/8, color(200, 65, 75), color(230, 115, 120), not_quite);
 
-  let btn7 = new Button(width/4, 3*height/4, height/3, height/8, color(200, 65, 75), color(230, 115, 120), rock_n_roll);
-  let btn8 = new Button(width/2, 3*height/4, height/3, height/8,color(200, 65, 75), color(230, 115, 120), stupid_pc);
-  let btn9 = new Button(3*width/4, 3*height/4, height/3, height/8, color(200, 65, 75), color(230, 115, 120), yawned);
+  let btn7 = new Button(width/4, 3*height/4-30, height/3, height/8, color(200, 65, 75), color(230, 115, 120), rock_n_roll);
+  let btn8 = new Button(width/2, 3*height/4-30, height/3, height/8,color(200, 65, 75), color(230, 115, 120), stupid_pc);
+  let btn9 = new Button(3*width/4, 3*height/4-30, height/3, height/8, color(200, 65, 75), color(230, 115, 120), yawned);
 
   //button arrays
   buttons_top.push(btn1);
@@ -97,9 +111,78 @@ function setup() {
   buttons_bottom.push(btn7);
   buttons_bottom.push(btn8);
   buttons_bottom.push(btn9);
+
+  // play whac-a-mole
+  playBtn = document.querySelector('#play-button');
+  playBtn.onclick = function() {
+    play = true;
+    playBtn.disabled = true;
+    startGame();
+  };
+}
+
+function startGame() {
+  result = 0;
+  currentTime = 30;
+  countdownTimer = setInterval(countDown, 1000);
+  timer = setInterval(randomBtn, 1000);
+} 
+
+function randomBtn() {
+  //start fresh
+  refreshButtons();
+
+  //set random button
+  let random = Math.floor(Math.random() * 10) % 9;
+  if(Math.floor(random / 3) == 0) {
+    buttons_top[random % 3].colour = color(0, 0, 155);
+    buttons_top[random % 3].accent = color(0, 0, 255);
+    buttons_top[random % 3].mole = true;
+  }
+  if(Math.floor(random / 3) == 1) {
+    buttons_middle[random % 3].colour = color(0, 0, 155);
+    buttons_middle[random % 3].accent = color(0, 0, 255);
+    buttons_middle[random % 3].mole = true;
+  }
+  if(Math.floor(random / 3) == 2) {
+    buttons_bottom[random % 3].colour = color(0, 0, 155);
+    buttons_bottom[random % 3].accent = color(0, 0, 255);
+    buttons_bottom[random % 3].mole = true;
+  }
+}
+
+function countDown() {
+  currentTime--;
+  const timeLeft = document.querySelector('#time');
+  timeLeft.textContent = currentTime;
+  if (currentTime == 0) {
+    play = false;
+    clearInterval(countdownTimer);
+    clearInterval(timer);
+    alert("GAME OVER!");
+    refreshButtons();
+    playBtn.disabled = false;
+  }
+}
+
+function refreshButtons() {
+  for (let i = 0; i < buttons_top.length; i++) {
+    buttons_top[i].colour = color(200, 65, 75);
+    buttons_top[i].accent = color(230, 115, 120);
+    buttons_top[i].mole = false;
+    
+    buttons_middle[i].colour = color(200, 65, 75);
+    buttons_middle[i].accent = color(230, 115, 120);
+    buttons_middle[i].mole = false;
+
+    buttons_bottom[i].colour = color(200, 65, 75);
+    buttons_bottom[i].accent = color(230, 115, 120);
+    buttons_bottom[i].mole = false;
+  }
 }
 
 function mousePressed() {
+  
   for (let i = 0; i < buttons_top.length; i++) {
     buttons_top[i].clicked(mouseX, mouseY);
     buttons_middle[i].clicked(mouseX, mouseY);
@@ -109,9 +192,9 @@ function mousePressed() {
 
 function mouseReleased() {
   for (let i = 0; i < buttons_top.length; i++) {
-    buttons_top[i].y = height/4;
-    buttons_middle[i].y = height/2;
-    buttons_bottom[i].y = 3*height/4;
+    buttons_top[i].y = height/4-30;
+    buttons_middle[i].y = height/2-30;
+    buttons_bottom[i].y = 3*height/4-30;
   }
 }
 
